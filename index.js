1fbd9afeb63ee0395c2ffc1aed82034f070771b5
github username <github email address>
2020-06-28 00:59:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+const map = (arr, callback) => {
+    const newArr = []
+    arr.forEach(e => {
+        newArr.push(callback(e))
+    })
+    return newArr;
+}
+
+const reduce = (arr, callback, startingPoint) => {
+    let result = (!!startingPoint) ? startingPoint : arr[0]
+    let i = (!!startingPoint) ? 0 : 1
+
+    for (; i < arr.length; i++ ){
+        result = callback(arr[i], result)
+    }
+    return result;
+}
